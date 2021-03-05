1450d0ae4a556b8a0c6aee48b126eb0995612307
xElGrego <elgrego977@gmail.com>
2021-03-05 13:31:07 -0500

    Inicio del proyecto - Grego Cofee UI

---
+++ android/app/src/main/kotlin/com/example/proyecto_coffeui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyecto_coffeui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:proyecto_coffeui/app/constants/constants.dart';
+
+import 'app/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    //ScreenUtilInit: Widget que ayuda a configurar el tamaño y fuente de la pantalla.
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: () => GetMaterialApp(
+        defaultTransition: Transition.cupertino,
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+        title: 'Grego Coffe- Ui',
+        theme: ThemeData(
+          scaffoldBackgroundColor: kFadedScaffoldColor,
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+      ),
+    );
+  }
+}
